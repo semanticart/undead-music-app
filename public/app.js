17f6f280a2fe4002e01380f1c0ad8abf3160677e
Jeffrey Chupp <jeff@semanticart.com>
2010-01-15 19:36:00 -0600

    more styling changes

--- public/app.js
+++ public/app.js
@@ -21,11 +21,18 @@ $(document).ready(function(){
   $.preLoadImages('/1h.jpg', '/2h.jpg');
 });
 
-function deselectExcept(keep){
+function selectOpt(keep){
   $.each(options, function() {
+    var el = $('#option_button_' + this);
     if (this != keep){
+      var normalImg = NormalImgOf(el.attr("src"));
+      el.attr("src", normalImg);
       $('#option_' + this).hide();
-      $('#option_button_' + this).css('background-image', 'url(/' + this + 'n.jpg)').css('border', '0px').css('margin-left', '0px');
+    }
+    else{
+      var hoverImg = HoverImgOf($(el).attr("src"));
+      $(el).attr("src", hoverImg);
+      $('#option_' + this).show();
     }
   });
 }
@@ -33,9 +40,32 @@ function deselectExcept(keep){
 function showOpt(number){
   if (number != current){
     current = number;
-    deselectExcept(number);
-    $('#option_' + number).show();
-    $('#option_button_' + number).css('background-image', 'url(/' + number + 'h.jpg)').css('border-left', '2px solid #37921d').css('margin-left', '-2px');
+    selectOpt(number);
   }
+}
+
+//borrowed from http://www.yart.com.au/Resources/jQuery/jQuery---Auto-replace-image-on-hover.aspx until I work on another version
+
+$(document).ready(function(){
+   // Change the image of hoverable images
+   $(".imgHoverable").hover( function() {
+       var hoverImg = HoverImgOf($(this).attr("src"));
+       $(this).attr("src", hoverImg);
+     }, function() {
+       if (parseInt(this.id.split('option_button_')[1]) != current){
+         var normalImg = NormalImgOf($(this).attr("src"));
+         $(this).attr("src", normalImg);
+       }
+     }
+   );
+});
+
+function HoverImgOf(filename){
+   var re = new RegExp("(.+)n\\.(gif|png|jpg)", "g");
+   return filename.replace(re, "$1h.$2");
+}
+function NormalImgOf(filename){
+   var re = new RegExp("(.+)h\\.(gif|png|jpg)", "g");
+   return filename.replace(re, "$1n.$2");
 }
 
